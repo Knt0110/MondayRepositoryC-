578628bccef2138cf985906049b85ceac41ae63b
MATAYOSHI RYO <158718894+ryo-cpu@users.noreply.github.com>
2026-07-13 11:30:28 +0900

    Lv5までじゃんけん

--- 20260706_Prac2/20260706_Function2_津留健翔.cpp
+++ 20260706_Prac2/20260706_Function2_津留健翔.cpp
@@ -11,6 +11,12 @@ void Game()
 	int playerLv{ PlayerStatus::START_LV };
 	int playerExp{ 0 };
 
+	int round{ 0 };
+	int playerHand, cpuHand;
+
+	//乱数初期化
+	srand((unsigned int)time(NULL));
+
 	//インフォメーション
 	cout << "＝＝＝＝＝＝＝＝じゃんけんゲーム＝＝＝＝＝＝＝＝\n"
 		<< "CPUとじゃんけんを行います。\n"
@@ -21,13 +27,39 @@ void Game()
 	while (true)
 	{
 		//じゃんけんバトル
-		Battle();
+		cout << format("第{}ラウンド", round + 1) << endl;
+		StatusOpen(playerLv, playerExp);
 
-		//経験値獲得
-		playerExp += GetExp(playerExp, GameSys::GET_EXP_MIN, GameSys::GET_EXP_MAX);
+		//入力
+		cout << "1: グー 2: チョキ 3:パー\n";
+		playerHand = InputCheck(GameSys::SELECT_MIN,GameSys::SELECT_MAX);
 
-		//レベルアップ
-		LvUp(playerLv, playerExp, PlayerStatus::LVUP_EXP);
+		//CPUの手
+		cpuHand = rand() % GameSys::SELECT_MAX + GameSys::SELECT_MIN;
+		switch (cpuHand)
+		{
+		case Hands::rock:
+			cout << "CPU: グー\n";
+			break;
+		case Hands::scissor:
+			cout << "CPU: チョキ\n";
+			break;
+		case Hands::paper:
+			cout << "CPU: パー\n";
+		}
+
+		//勝敗
+		Result result = Judgement(playerHand, cpuHand);
+
+		//勝利時
+		if (result == playerWin)
+		{
+			//経験値獲得
+			playerExp += GetExp(playerExp, GameSys::GET_EXP_MIN, GameSys::GET_EXP_MAX);
+
+			//レベルアップ
+			playerLv = LvUp(playerLv, playerExp, PlayerStatus::LVUP_EXP);
+		}
 
 		//終了条件
 		if (playerLv >= PlayerStatus::END_LV)
@@ -41,9 +73,6 @@ void Game()
 
 int GetExp(int exp, int expMin, int expMax)
 {
-	//乱数初期化
-	srand((unsigned int)time(NULL));
-
 	//変数宣言 & 獲得経験値
 	int getExp{ rand() % (expMax)+expMin };
 
@@ -53,21 +82,65 @@ int GetExp(int exp, int expMin, int expMax)
 	return getExp;
 }
 
-int LvUp(int lv, int exp, int maxExp)
+int LvUp(int &lv, int &exp, int maxExp)
 {
 	if (exp >= maxExp)
 	{
 		lv++;
 		exp -= maxExp;
-		cout << format("レベルアップ！\nLv{}になりました。", lv) << endl;
+		cout << format("レベルアップ！\nLv{}になりました。\n", lv) << endl;
 	}
 	return lv;
 }
 
-void Battle()
-{
-	//変数宣言
-	int round{ 0 };
 
-	cout << format("第{}ラウンド", round + 1) << endl;
+void StatusOpen(int playerLv,int playerExp)
+{
+	cout << "========================\n"
+		<< "Lv : " << playerLv
+		<< "\nEXP : " << playerExp
+		<< "\n========================" << endl;
+}
+
+int InputCheck(int min, int max)
+{
+	int num;
+
+	while (true)
+	{
+		cout << "入力: ";
+		cin >> num;
+
+		if (num < min || num > max)
+		{
+			cout << "範囲外です。再度入力してください。\n";
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return num;
+}
+
+Result Judgement(int player, int cpu)
+{
+	int num = (cpu - player + GameSys::HAND_NUM) % GameSys::HAND_NUM;
+
+	// 計算結果を enum Result に変換
+	Result result = static_cast<Result>(num);
+
+	// 結果を画面に表示する
+	if (result == playerWin) {
+		cout << "あなたの勝ち！\n";
+	}
+	else if (result == cpuWin) {
+		cout << "CPUの勝ち....\n";
+	}
+	else {
+		cout << "あいこ..\n";
+	}
+	cout << "\n";
+	return result;
 }
--- 20260706_Prac2/20260706_Header2_津留健翔.h
+++ 20260706_Prac2/20260706_Header2_津留健翔.h
@@ -1,13 +1,66 @@
 #pragma once
+//じゃんけんの手
+enum Hands
+{
+	rock = 1,
+	scissor = 2,
+	paper = 3
+};
+
+//勝敗結果
+enum Result
+{
+	draw,
+	playerWin,
+	cpuWin
+};
 
 //グローバル関数化
+
+/// <summary>
+/// ゲームの関数
+/// </summary>
 void Game();
 
+/// <summary>
+/// 経験値獲得関数
+/// </summary>
+/// <param name="exp"></param>
+/// <param name="expMin"></param>
+/// <param name="expMax"></param>
+/// <returns></returns>
 int GetExp(int exp, int expMin, int expMax);
 
-int LvUp(int lv, int exp, int maxExp);
+/// <summary>
+/// レベルアップ関数
+/// </summary>
+/// <param name="lv"></param>
+/// <param name="exp"></param>
+/// <param name="maxExp"></param>
+/// <returns></returns>
+int LvUp(int &lv, int &exp, int maxExp);
 
-void Battle();
+/// <summary>
+/// 現在のステータスを確認
+/// </summary>
+/// <param name="playerLv"></param>
+/// <param name="playerExp"></param>
+void StatusOpen(int playerLv, int playerExp);
+
+/// <summary>
+/// 入力チェック
+/// </summary>
+/// <param name="min"></param>
+/// <param name="max"></param>
+/// <returns></returns>
+int InputCheck(int min, int max);
+
+/// <summary>
+/// じゃんけんの勝敗
+/// </summary>
+/// <param name="player"></param>
+/// <param name="cpu"></param>
+Result Judgement(int player, int cpu);
 
 //プレイヤーのステータス
 namespace PlayerStatus
@@ -24,4 +77,6 @@ namespace GameSys
 	const int SELECT_MAX{ 3 };
 	const int GET_EXP_MIN{ 1 };
 	const int GET_EXP_MAX{ 15 };
+
+	const int HAND_NUM{ 3 };
 }
